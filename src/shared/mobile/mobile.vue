154478a823000381b4b59182e00d781bb62dd32b
Evan Peuvergne <evanpeuvergne@gmail.com>
2017-05-11 22:27:50 +0200

    Update projects config

---
+++ src/shared/mobile/mobile.vue
@@ -0,0 +1,68 @@
+
+
+  <script>
+    
+
+    /* Dependencies */
+
+    import $ from 'jquery';
+    import _ from 'lodash';
+
+
+
+    
+    /* Component */
+
+    var component = { name: 'mobile', methods: {} };
+
+    component.data = function () {
+      return {};
+    };
+
+    component.components = {};
+
+
+    
+
+    /* Hooks */
+
+    component.created = function () {
+
+         
+
+    };
+
+    component.mounted = function () {
+
+      
+
+    };
+
+
+
+
+    /* Export */
+
+    export default component;
+
+
+  </script>
+
+
+  <template lang="jade">
+    
+    //- Root
+    .mobile
+
+      h1 Hello
+    
+
+  </template>
+
+
+  
+  <style lang="stylus" scoped>
+    
+    
+
+  </style>
